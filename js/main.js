4fb19bf031f14f658af147daac64d2229f6b5894
Kelly Landers <kelly.landers@sq1.com>
2014-09-04 18:30:02 -0500

    jacking around with templates

--- js/main.js
+++ js/main.js
@@ -5,15 +5,25 @@ var n = d.getDay();
 
 
 var data = {
-	headerTitle: "Shop Page",
-	weekDay: d
+	headerTitle : 'Shop Page',
+	weekDay: d,
+	bodyTitle : 'This is from the bodyTitle value',
+	bodyContent : 'Same here nucka',
+	footerTitle : 'Footer Motherfucker',
+	footerContent : 'This is being loaded from the data object'
 }
 
-var theTemplateScript = $("#header").html();
+var headerTemplate = $('#header').html();
+var bodyTemplate   = $('#body').html();
+var footerTemplate = $('#footer').html();
 
-var theTemplate = Handlebars.compile(theTemplateScript);  
+var headerCompile = Handlebars.compile(headerTemplate);  
+var bodyCompile = Handlebars.compile(bodyTemplate);  
+var footerCompile = Handlebars.compile(footerTemplate);  
 
-$(document.body).append(theTemplate(data));
+$(document.body).append(headerCompile(data));
+$(document.body).append(bodyCompile(data));
+$(document.body).append(footerCompile(data));
  
 
 });
