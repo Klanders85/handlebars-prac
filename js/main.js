fb8a37bae418ad26faec39ea337f44d5ef7e5f0e
Kelly Landers <kelly.landers@sq1.com>
2014-10-23 07:54:05 -0500

    styling (y)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
 // data goes here that will be dynamic -- JSON FORMAT!
-var data = {
+var bandData = {
 	"bandName"     :[  "Mudvayne",
-					   '<img src="http://placehold.it/350x150">',
+					   "Tool",
 					   "APC",
 					   "Mudvayne",
 					],
@@ -16,12 +16,12 @@ var data = {
 }
 
 //save templates to var
-var contentTemplate   = $('#content').html();
+var contentTemplate   = $('#bandContent').html();
 
 //compile templates
 var contentCompile = Handlebars.compile(contentTemplate);  
 
 //add templates to the HTML
-$(document.body).append(contentCompile(data));
+$(document.body).append(contentCompile(bandData));
 
 });
